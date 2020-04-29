28881108a3616d0e3b628d5bca66be80848ebe62
Vanda Adams <vandamadams@gmail.com>
2020-04-29 13:17:52 +0100

    devise setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
+
+  resources :users, only: [:show, :edit, :update] do
+    resources :items, only: [:new, :create, :update]
+    resources :events, only: [:new, :create, :update]
+  end
+
+  resources :items, only: :index
+  resources :events, only: :index
+
+  get "/profile", to: 'pages#profile'
 end
