30b1c13614be372e229ebf655d403bd39c771dce
Vanda Adams <vandamadams@gmail.com>
2020-04-30 19:51:40 +0100

    added foreign key to joint table

--- db/migrate/20200430184953_create_user_items.rb
+++ db/migrate/20200430184953_create_user_items.rb
@@ -1,6 +1,8 @@
 class CreateUserItems < ActiveRecord::Migration[6.0]
   def change
     create_table :user_items do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :item, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_30_091214) do
+ActiveRecord::Schema.define(version: 2020_04_30_184953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,8 +31,12 @@ ActiveRecord::Schema.define(version: 2020_04_30_091214) do
   end
 
   create_table "user_items", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "item_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["item_id"], name: "index_user_items_on_item_id"
+    t.index ["user_id"], name: "index_user_items_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -49,4 +53,6 @@ ActiveRecord::Schema.define(version: 2020_04_30_091214) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "user_items", "items"
+  add_foreign_key "user_items", "users"
 end
