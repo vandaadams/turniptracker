dfa54e03cd00ac643bc8e7b2814829c3b9632e22
Vanda Adams <vandamadams@gmail.com>
2020-04-30 15:06:34 +0100

    initial controller setup

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,41 @@
 class EventsController < ApplicationController
+  def index
+    @events = Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.user = current_user
+  end
+
+  def edit
+    @events = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    @event.update(event_params)
+    redirect to event_path(@event)
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.delete
+    redirect_to events_path
+  end
+
+  private
+
+  def event_params
+    params.require(:item).permit(:name, :date, :description)
+  end
+  
 end
