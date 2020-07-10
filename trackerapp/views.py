1411ba03daa511b6d40d0d9dcf434f3f35b95d45
Vanda Adams <vandamadams@gmail.com>
2020-07-10 11:03:19 +0100

    added views

--- trackerapp/views.py
+++ trackerapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    context = {}
+    return render(request, 'trackerapp/home.html', context)
+
+def profile(request):
+    context = {}
+    return render(request, 'trackerapp/profile.html', context)
