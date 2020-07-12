58bf70ddb925218f92fdb00975aab01f75234b73
Vanda Adams <vandamadams@gmail.com>
2020-07-12 17:27:16 +0100

    added turnip model

--- trackerapp/models.py
+++ trackerapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Turnip(models.Model):
+    day_choice = (
+        ('Monday', 'Monday'),
+        ('Tuesday', 'Tuesday'),
+        ('Wednesday', 'Wednesday'),
+        ('Thursday', 'Thursday'),
+        ('Friday', 'Friday'),
+        ('Saturday', 'Saturday'),
+    )
+    day = models.CharField(blank=False, choices=day_choice)
+    time_choice = (
+        ('Morning', 'Morning'),
+        ('Evening', 'Evening'),
+    )
+    time = models.CharField(blank=False, choices=time_choice)
+    price = models.IntegerField(blank=False)
