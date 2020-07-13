8bdd9e29f7924c41481723a3f09f4340ffbb9984
Vanda Adams <vandamadams@gmail.com>
2020-07-13 20:31:04 +0100

    added logout to navbar when user is signed in

--- trackerapp/urls.py
+++ trackerapp/urls.py
@@ -5,7 +5,7 @@ from .views import PriceList
 urlpatterns = [
 	path('register/', views.registerPage, name="register"),
 	path('login/', views.loginPage, name="login"),
-	path('logout/', views.logoutUser name="logout"),
+	path('logout/', views.logoutUser, name="logout"),
 	path('', views.home, name="home"),
 	path('profile/', views.profile, name="profile"),
 	path('form/', PriceList.as_view(), name="form")
