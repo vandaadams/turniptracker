3e3465601d666ff5762fb8b24bed4f59f412499f
Vanda Adams <vandamadams@gmail.com>
2020-07-13 17:14:35 +0100

    added register and login paths

--- trackerapp/urls.py
+++ trackerapp/urls.py
@@ -3,6 +3,8 @@ from . import views
 from .views import PriceList
 
 urlpatterns = [
+	path('register/', views.registerPage, name="register"),
+	path('login/', views.loginPage, name="login"),
 	path('', views.home, name="home"),
 	path('profile/', views.profile, name="profile"),
 	path('form/', PriceList.as_view(), name="form")
