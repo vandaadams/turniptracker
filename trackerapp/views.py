90eba19572a04b62bb3d2d5ef23f661793720bd8
Vanda Adams <vandamadams@gmail.com>
2020-07-13 17:14:10 +0100

    added register and login views

--- trackerapp/views.py
+++ trackerapp/views.py
@@ -5,6 +5,13 @@ from .models import Turnip
 from .forms import TurnipForm
 
 # Create your views here.
+def registerPage(request):
+    context = {}
+    return render(request, 'accounts/register.html', context)
+
+def loginPage(request):
+    context = {}
+    return render(request, 'accounts/login.html', context)
 
 class PriceList(View):
     def get(self, request):
