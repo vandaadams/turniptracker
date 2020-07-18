e85e7d476fcac8d41b70e251026fa24db3f5bc9e
Vanda Adams <vandamadams@gmail.com>
2020-07-18 12:45:06 +0100

    added new secret key

--- turniptracker/settings.py
+++ turniptracker/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gl$tu+&6cmc1fb!-dw#6j#_c+sw%zyna(kk=n@mafv*5l58v8!'
+SECRET_KEY = os.environ.get('TRACKERAPP_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
